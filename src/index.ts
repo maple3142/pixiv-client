865565b23fee247919d1430c53359358686cbe69
maple3142 <kirby741852963@gmail.com>
2018-11-30 22:57:45 +0800

    Support some desktop api without login

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,17 @@ export { PixivMobileApi } from './mobile'
 import * as got from 'got'
 import * as fs from 'fs'
 import * as path from 'path'
-import { Duplex } from 'stream';
+import { Duplex } from 'stream'
 
-export function downloadAsStream(url: string):Duplex {
+export function downloadAsStream(url: string): Duplex {
 	return got.stream(url, {
 		headers: {
 			Referer: 'https://www.pixiv.net/'
 		}
 	})
 }
-export function downloadToLocal(url: string, absfilepath= '') {
-	if(!path.isAbsolute(absfilepath)){
+export function downloadToLocal(url: string, absfilepath = '') {
+	if (!path.isAbsolute(absfilepath)) {
 		throw new TypeError('path must be absolute path!')
 	}
 	return new Promise((res, rej) => {
